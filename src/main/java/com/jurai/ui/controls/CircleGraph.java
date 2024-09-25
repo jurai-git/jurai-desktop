ac42a22bc69e1f7d2be691e50489f7667cc97cc3
mgcvale <miguelcvalealt@gmail.com>
2024-09-25 11:30:19 -0300

    finish circlegraph styling

--- src/main/java/com/jurai/ui/controls/CircleGraph.java
+++ src/main/java/com/jurai/ui/controls/CircleGraph.java
@@ -1,29 +1,21 @@
 package com.jurai.ui.controls;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 
 public class CircleGraph extends StackPane {
-    private double lineWidth;
     private double percentage;
 
-    private Path path;
-    private final Arc arc;
-    private Rectangle base;
+    private Circle base;
+    private Arc percentageArc;
     private final Label percentageLabel;
 
-    public CircleGraph(double lineWidth, double percentage) {
-        this.lineWidth = lineWidth;
+    public CircleGraph(double percentage) {
         getStyleClass().add("circle-graph");
         this.percentage = percentage;
         percentageLabel = new Label(String.format("%.1f%%", percentage * 100));
-        path = new Path();
-        arc = new Arc();
         initGraphics();
     }
 
@@ -31,6 +23,7 @@ public class CircleGraph extends StackPane {
         this.widthProperty().addListener((observableValue, number, t1) -> redraw());
         this.heightProperty().addListener((observableValue, number, t1) -> redraw());
         redraw();
+        getChildren().setAll(percentageArc, percentageLabel);
     }
 
     private void redraw() {
@@ -39,38 +32,19 @@ public class CircleGraph extends StackPane {
         double radius = Math.min(getWidth(), getHeight()) * 0.4;  // Radius of the circle
         double endAngle = percentage * 360;
         percentageLabel.setText(String.format("%.1f%%", percentage * 100));
-        double endAngleRadians = Math.toRadians(endAngle - 90);
-        /*
-        This -90 has to be there because the trigonometric functions start in the
-        right side of the cartesian plane. However, since we have to make it start from
-        the top in our app, we need to subtract 90 degrees.
-         */
-
-        double startX = centerX;
-        double startY = centerY - radius;
-        double endX = centerX + radius * Math.cos(endAngleRadians);
-        double endY = centerY + radius * Math.sin(endAngleRadians);
         
         // Create an Arc instead of using Path with ArcTo
-        arc.setCenterX(centerX);
-        arc.setCenterY(centerY);
-        arc.setRadiusX(radius);
-        arc.setRadiusY(radius);
-        arc.setStartAngle(90);
-        arc.setLength(-endAngle);
-        arc.setFill(null);
-        arc.setStroke(Color.rgb(83, 156, 212));
-        arc.setStrokeWidth(lineWidth);
-        arc.setType(ArcType.OPEN);
-        arc.setStrokeLineCap(StrokeLineCap.ROUND);
+        percentageArc = new Arc(centerX, centerY, radius, radius, 90, -endAngle);
+        percentageArc.getStyleClass().add("percentage");
+        percentageArc.setStroke(Color.rgb(83, 156, 212));
+        percentageArc.setType(ArcType.OPEN);
+        percentageArc.setStrokeLineCap(StrokeLineCap.ROUND);
 
-        System.out.println(path.getElements());
-
-        applyCss();
-        layout();
+        base = new Circle(centerX, centerY, radius);
+        base.getStyleClass().add("base");
 
         getChildren().clear();
-        getChildren().setAll(arc, percentageLabel);
+        getChildren().setAll(base, percentageArc, percentageLabel);
     }
 
     @Override
@@ -86,14 +60,5 @@ public class CircleGraph extends StackPane {
 
     public double getPercentage() {
         return percentage;
-    }
-
-    public void setLineWidth(double lineWidth) {
-        this.lineWidth = lineWidth;
-        redraw();
-    }
-
-    public double getLineWidth() {
-        return lineWidth;
     }
 }
--- src/main/java/com/jurai/ui/menus/DemandaAnalysisDashboardMenu.java
+++ src/main/java/com/jurai/ui/menus/DemandaAnalysisDashboardMenu.java
@@ -3,36 +3,21 @@ package com.jurai.ui.menus;
 import com.jurai.ui.controls.CircleGraph;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 
 public class DemandaAnalysisDashboardMenu extends AbstractMenu<HBox> {
     private HBox content;
     private CircleGraph circleGraph;
+    private Label circleGraphLabel;
 
     @Override
     protected void initControls() {
         content = new HBox();
-        circleGraph = new CircleGraph(8, 0.1);
+        circleGraph = new CircleGraph( 0.242);
         HBox.setHgrow(circleGraph, Priority.ALWAYS);
-        content.getChildren().add(circleGraph);
-        Task<Void> task = new Task<>() {
-            @Override
-            protected Void call() throws Exception {
-                while (true) {
-                    Thread.sleep(100);
-                    Platform.runLater(() -> {
-                        double newPercentage = circleGraph.getPercentage() + 0.01;
-                        if (newPercentage >= 1) {
-                            newPercentage = 0.01;
-                        }
-                        circleGraph.setPercentage(newPercentage);
-                    });
-                }
-            }
-        };
-
-        new Thread(task).start();
+        content.getChildren().addAll(circleGraph);
     }
 
     @Override
